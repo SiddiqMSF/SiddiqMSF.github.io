ec543c29e97f56c6e30832203c25c2d796f10af1
RandQuran <siddiqmutarji.m@gmail.com>
2023-10-12 13:42:37 +0300

    a

--- script/lang.js
+++ script/lang.js
@@ -1,29 +1,12 @@
-window.onload = function() {
-    document.querySelectorAll('[data-i18n]').forEach(elem => {
-        if (elem.childNodes[0].nodeType === Node.TEXT_NODE) {
-            elem.setAttribute('data-original', elem.childNodes[0].textContent);
-        }
-    });
-}
-
 function applyTranslation(lang) {
-    if (lang === 'en') {
-        document.querySelectorAll('[data-i18n]').forEach(elem => {
-            let original = elem.getAttribute('data-original');
-            if (elem.childNodes[0].nodeType === Node.TEXT_NODE) {
-                elem.childNodes[0].textContent = original;
-            }
-        });
-    } else {
-        fetch('/languages/' + lang + '.json')
-            .then(response => response.json())
-            .then(data => {
-                document.querySelectorAll('[data-i18n]').forEach(elem => {
-                    let key = elem.getAttribute('data-i18n');
-                    if (elem.childNodes[0].nodeType === Node.TEXT_NODE) {
-                        elem.childNodes[0].textContent = data[key] || elem.childNodes[0].textContent;
-                    }
-                });
+    fetch('/languages/' + lang + '.json')
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll('[data-i18n]').forEach(elem => {
+                let key = elem.getAttribute('data-i18n');
+                if (elem.childNodes[0].nodeType === Node.TEXT_NODE) {
+                    elem.childNodes[0].textContent = data[key] || elem.childNodes[0].textContent;
+                }
             });
-    }
+        });
 }
