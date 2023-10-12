2c668be001af04c937c36b74ab68001593336516
SiddiqMSF <146487742+SiddiqMSF@users.noreply.github.com>
2023-10-12 22:15:48 +0300

    Update download.js

--- script/download.js
+++ script/download.js
@@ -1,18 +1,22 @@
-document.getElementById('downloadButton').addEventListener('click', () => {
-    const csvContent = Object.entries(yearlyEntries).flatMap(([year, entries]) =>
-        entries.map(entry => `${year},${entry.date},${entry.book},${entry.price}`)
-    ).join('\n');
+document.getElementById('downloadButton').addEventListener('click', () => { 
+    const csvContent = Object.entries(yearlyEntries).flatMap(([year, entries]) => 
+        entries.map(entry => `${year},${entry.date},${entry.book},${entry.price}`) 
+    ).join('\n'); 
 
-    const blob = new Blob([`Year,Date,Book,Price\n${csvContent}`], { type: 'text/csv;charset=utf-8;' });
-    const url = URL.createObjectURL(blob);
+    // Add UTF-8 BOM at the start of the string
+    const BOM = "\uFEFF";
+    const csvWithBOM = BOM + `Year,Date,Book,Price\n${csvContent}`;
 
-    const link = Object.assign(document.createElement('a'), {
-        href: url,
-        download: 'entries.csv',
-        style: { visibility: 'hidden' },
-    });
+    const blob = new Blob([csvWithBOM], { type: 'text/csv;charset=utf-8;' }); 
+    const url = URL.createObjectURL(blob); 
 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    const link = Object.assign(document.createElement('a'), { 
+        href: url, 
+        download: 'entries.csv', 
+        style: { visibility: 'hidden' }, 
+    }); 
+
+    document.body.appendChild(link); 
+    link.click(); 
+    document.body.removeChild(link); 
 });
