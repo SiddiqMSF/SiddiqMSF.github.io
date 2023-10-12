e85074be89fc4679377a5ab1a55acbb356d336ce
RandQuran <siddiqmutarji.m@gmail.com>
2023-10-12 13:09:48 +0300

    g

--- script/lang.js
+++ script/lang.js
@@ -1,10 +1,10 @@
 function applyTranslation(lang) {
-    fetch('languages/' + lang + '.json')
+    fetch('/languages/' + lang + '.json')
         .then(response => response.json())
         .then(data => {
             document.querySelectorAll('[data-i18n]').forEach(elem => {
                 let key = elem.getAttribute('data-i18n');
-                elem.textContent = data[key] || elem.textContent;
+                elem.childNodes[0].textContent = data[key] || elem.childNodes[0].textContent;
             });
         });
 }
