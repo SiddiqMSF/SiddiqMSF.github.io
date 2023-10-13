4a318f80d7d87bc51172bb57d392a86028d8ff44
RandQuran <siddiqmutarji.m@gmail.com>
2023-10-13 18:46:09 +0300

    Update download.js

--- script/download.js
+++ script/download.js
@@ -1,15 +1,9 @@
 document.getElementById('downloadButton').addEventListener('click', () => {
-    const entries = Object.entries(yearlyEntries).flatMap(([year, entries]) =>
-        entries.map(entry => ({Year: year, Date: entry.date, Book: entry.book, Price: entry.price}))
-    );
-
-    /* create a new blank workbook and worksheet */
     const wb = XLSX.utils.book_new();
-    const ws = XLSX.utils.json_to_sheet(entries);
-
-    /* add the worksheet to the workbook */
-    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-
-    /* write the workbook and force a download */
+    XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(
+        Object.entries(yearlyEntries).flatMap(([year, entries]) =>
+            entries.map(entry => ({Year: year, Date: entry.date, Book: entry.book, Price: entry.price}))
+        )
+    ), "Sheet1");
     XLSX.writeFile(wb, 'entries.xlsx');
 });
