8f2558ae9a777d4c6d453d80683342c28a0f8ef3
RandQuran <siddiqmutarji.m@gmail.com>
2023-10-13 13:21:20 +0300

    ngf

--- script/download.js
+++ script/download.js
@@ -1,21 +1,26 @@
-document.getElementById('downloadButton').addEventListener('click', () => { 
-    const csvContent = Object.entries(yearlyEntries).flatMap(([year, entries]) => 
-        entries.map(entry => `${year},${entry.date},${entry.book},${entry.price}`) 
-    ).join('\n'); 
+import XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
-    const BOM = "\uFEFF";
-    const csvWithBOM = BOM + `Year,Date,Book,Price\n${csvContent}`;
+document.getElementById('downloadButton').addEventListener('click', () => {
+    const wb = XLSX.utils.book_new();
 
-    const blob = new Blob([csvWithBOM], { type: 'text/csv;charset=utf-8;' }); 
-    const url = URL.createObjectURL(blob); 
+    const data = Object.entries(yearlyEntries).flatMap(([year, entries]) =>
+        entries.map(entry => [year, entry.date, entry.book, entry.price])
+    );
 
-    const link = Object.assign(document.createElement('a'), { 
-        href: url, 
-        download: 'entries.csv', 
-        style: { visibility: 'hidden' }, 
-    }); 
+    data.unshift(['Year', 'Date', 'Book', 'Price']);
 
-    document.body.appendChild(link); 
-    link.click(); 
-    document.body.removeChild(link); 
+    const ws = XLSX.utils.aoa_to_sheet(data);
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+    }
+
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), 'entries.xlsx');
 });
